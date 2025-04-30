dcef9173901f407df59f92de21fbb100d27634af
YaminHossain <yhossain192117@bscse.uiu.ac.bd>
2025-05-01 05:28:40 +0600

    sign up validation added successfully

--- src/app/signup/Components/SignUpPageForm.jsx
+++ src/app/signup/Components/SignUpPageForm.jsx
@@ -1,18 +1,18 @@
 "use client";
-import { useState } from "react";
-import { set, useForm } from "react-hook-form";
-import { FaRegCircleCheck, FaRegCircleXmark } from "react-icons/fa6";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import PasswordValidation from "./PasswordValidation";
 
 const SignUpPageForm = () => {
   const { register, handleSubmit } = useForm();
 
-  // ========password field value============
+  // ========Password field value============
   const [passwordValue, setPasswordValue] = useState("");
   console.log(passwordValue);
 
-  // =================password validity check. For all validation===========================
-  const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(passwordValue);
-  if(isValidPassword) console.log("The password is valid");
+  // =======Password validity check for all validation========
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  console.log("Password Valid", isValidPassword);
 
   // Form submission handler
   const formHandler = (data) => console.log(data);
@@ -20,6 +20,21 @@ const SignUpPageForm = () => {
   // ==========Password Field on Focus=================
   const passwordFieldHandled = (e) => {
     setPasswordValue(e.target.value);
+  };
+  // ==========Confirm Password Field Event Handler=================
+  const confirmPasswordFieldHandled = (e) => {
+    const confirmPasswordValue = e.target.value;
+    // -----Setting Password validity-----
+    if (
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(
+        passwordValue
+      ) &&
+      passwordValue === confirmPasswordValue
+    ) {
+      setIsValidPassword(true);
+    } else {
+      setIsValidPassword(false);
+    }
   };
   return (
     <form onSubmit={handleSubmit(formHandler)} className="py-4">
@@ -56,71 +71,8 @@ const SignUpPageForm = () => {
         />
       </div>
       {/* ==========Password Validation=============== */}
-      {passwordValue.length > 0 && (
-        <div>
-          {/* -------validation 1----------- */}
-          <div>
-            {/^.{8,}$/.test(passwordValue) ? (
-              <p className="text-green-400 flex items-center gap-2">
-                <FaRegCircleCheck /> <span>At least 8 characters</span>
-              </p>
-            ) : (
-              <p className="text-red-400 flex items-center gap-2">
-                <FaRegCircleXmark /> <span>At least 8 characters</span>
-              </p>
-            )}
-          </div>
-          {/* --------validation 2----------- */}
-          <div>
-            {/^(?=.*[a-z]).*$/.test(passwordValue) ? (
-              <p className="text-green-400 flex items-center gap-2">
-                <FaRegCircleCheck /> <span>Lower case letters (a-z)</span>
-              </p>
-            ) : (
-              <p className="text-red-400 flex items-center gap-2">
-                <FaRegCircleXmark /> <span>Lower case letters (a-z)</span>
-              </p>
-            )}
-          </div>
-          {/* ----------validation 3---------- */}
-          <div>
-            {/^(?=.*[A-Z]).*$/.test(passwordValue) ? (
-              <p className="text-green-400 flex items-center gap-2">
-                <FaRegCircleCheck /> <span>Upper case letters (A-Z)</span>
-              </p>
-            ) : (
-              <p className="text-red-400 flex items-center gap-2">
-                <FaRegCircleXmark /> <span>Upper case letters (A-Z)</span>
-              </p>
-            )}
-          </div>
-          {/* ----------validation 4------------ */}
-          <div>
-            {/^(?=.*\d).*$/.test(passwordValue) ? (
-              <p className="text-green-400 flex items-center gap-2">
-                <FaRegCircleCheck /> <span>Numbers (0-9)</span>
-              </p>
-            ) : (
-              <p className="text-red-400 flex items-center gap-2">
-                <FaRegCircleXmark /> <span>Numbers (0-9)</span>
-              </p>
-            )}
-          </div>
-          {/* -------------validation 5------------- */}
-          <div>
-            {/^(?=.*[!@#$%^&*]).*$/.test(passwordValue) ? (
-              <p className="text-green-400 flex items-center gap-2">
-                <FaRegCircleCheck /> <span>Special characters (!@#$%^&*)</span>
-              </p>
-            ) : (
-              <p className="text-red-400 flex items-center gap-2">
-                <FaRegCircleXmark /> <span>Special characters (!@#$%^&*)</span>
-              </p>
-            )}
-          </div>
-        </div>
-      )}
-      {/* ===============Password validation ends here============== */}
+      <PasswordValidation passwordValue={passwordValue}></PasswordValidation>
+
       {/* ========confirm password field====== */}
       <div className="my-6">
         <label className="my-3 font-semibold text-gray-700">
@@ -132,11 +84,28 @@ const SignUpPageForm = () => {
           className="border border-gray-300 p-4 w-full  rounded-lg mt-3 focus:outline-0"
           placeholder="Enter your password again"
           type="password"
+          onChange={confirmPasswordFieldHandled}
         />
+        {isValidPassword ? (
+          <p className="text-green-400 mt-1.5">Good to go!</p>
+        ) : (
+          <p className="text-red-400 mt-1.5">Password did not match!</p>
+        )}
       </div>
-      <button type="submit" className="button-solid-red w-full">
-        Sign Up
-      </button>
+      {/* ===========Sign Up button======================= */}
+      {isValidPassword ? (
+        <button type="submit" className="button-solid-red w-full">
+          Sign Up
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="bg-[#ff3811] opacity-50 py-4 rounded-md text-white font-bold cursor-not-allowed w-full"
+          disabled
+        >
+          Sign Up
+        </button>
+      )}
     </form>
   );
 };
